2ab6809b36a7c2f42c14a33deedca7cf8ce1cdf5
priest4612 <prie4612@gmail.com>
2021-07-12 21:39:31 +0300

    Добавил дебагер в проект

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -26,3 +26,6 @@ if settings.DEBUG:
         settings.STATIC_URL,
         document_root=settings.STATIC_ROOT
     )
+
+    import debug_toolbar
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
